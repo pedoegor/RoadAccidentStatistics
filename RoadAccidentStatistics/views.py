dd38181f76891002a49f5dc178cbf755ce954c05
Nickolay Saveliev <nickolay.saveliev@gmail.com>
2014-04-02 20:28:31 +0400

    hierarchy view with dfs on region hierarchy

--- RoadAccidentStatistics/views.py
+++ RoadAccidentStatistics/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render_to_response
+from RoadAccidentStatistics.models import Region
 from utils.region_offset import RegionOffset
 
 
 def render_object_hierarchy(request):
-    regions = [RegionOffset("Region1", 0), RegionOffset("Region2", 1)]
+    def dfs(region, level, region_list):
+        region_list.append(RegionOffset(region.name, level))
+        for child in region.region_set.all():
+            dfs(child, level + 1, region_list)
+
+    regions = []
+    dfs(Region.objects.filter(parent=None)[0], 0, regions)
     return render_to_response('hierarchy.template.html', {'regions': regions})
 
 
