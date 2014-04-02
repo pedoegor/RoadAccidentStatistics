3dda53c3c82e8f9ad4f80058b5d0fd9aeaebeea9
Nickolay Saveliev <nickolay.saveliev@gmail.com>
2014-04-02 18:14:03 +0400

    new models

--- RoadAccidentStatistics/models.py
+++ RoadAccidentStatistics/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
 
-class StatSubject(models.Model):
+class Region(models.Model):
     name = models.CharField(max_length=100)
-    year = models.IntegerField()
-    deadNumber = models.IntegerField()
-    injuredNumber = models.IntegerField()
+    english_name = models.CharField(max_length=100)
     parent = models.ForeignKey("self", null=True, default=None)
 
     def __unicode__(self):
         parent_name = "None" if self.parent is None else self.parent.name
-        return u'Subject: %s, dead: %s, injured: %s, year: %s, parent: %s' % (self.name, self.deadNumber,
-                                                                              self.injuredNumber, self.year,
-                                                                              parent_name)
+        return u'Region: %s, parent: %s' % (self.name, parent_name)
+
+
+class RegionStat(models.Model):
+    region = models.ForeignKey(Region)
+    year = models.IntegerField()
+    deadNumber = models.IntegerField()
+    injuredNumber = models.IntegerField()
+
+    def __unicode__(self):
+        return u'RegionStat: %s, dead: %s, injured: %s, year: %s' % (self.region.name, self.deadNumber,
+                                                                              self.injuredNumber, self.year)
+
+
+class RegionPopulation(models.Model):
+    region = models.ForeignKey(Region)
+    year = models.IntegerField()
+    population = models.BigIntegerField()
+
+    def __unicode__(self):
+        return u'RegionPopulation: %s, population: %s, year: %s' % (self.region.name, self.population, self.year)
+
