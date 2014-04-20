b77a9b0311ec479f36c10fff857e911eaed5dab0
povelitel <7samarin.alexey7@gmail.com>
2014-04-20 20:40:34 +0400

    Translation -htmls corrected -translations.py added with map that contains internationalisation

---
+++ utils/translations.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+#only for .py  |||  .html are rewritten according to perfect translation
+international = {
+
+    #views.py
+    'title': {u'Ru' : u'Статистика ДТП', u'En' : u'Road accident statistics'},
+    'bubble_chart_title': {u'Ru' : u'Пузырьковая диаграмма', u'En' : u'Bubble chart'},
+    'trend_chart_title': {u'Ru' : u'График статистики ДТП с трендом', u'En' : u'Graph statistics of road accidents with the trend line'},
+    'pie_chart_title': {u'Ru' : u'Круговая диаграмма причин ДТП', u'En' : u'Pie chart reasons of road accidents'},
+    'sankey_chart_title': {u'Ru' : u'Потоковая диаграмма причин ДТП', u'En' : u'Sankey chart of reasons of road accidents'},
+    'parameters_title': {u'Ru' : u'Параметры', u'En' : u'Parameters'},
+    'information_header': {u'Ru' : u'Информация', u'En' : u'Information'},
+    'regions_title': {u'Ru' : u'Регионы', u'En' : u'Regions'},
+    'from_title': {u'Ru' : u'С', u'En' : u'From'},
+    'to_title': {u'Ru' : u'К', u'En' : u'To'},
+    'bubble_xAxis': {u'Ru' : u'Количество ДТП на 10 тыс. ед. ТС', u'En' : u'Road accidents per 10 thousand units of vehicles'},
+    'bubble_yAxis': {u'Ru' : u'Число пострадавших на 100 тыс. жителей', u'En' : u'The number of injured per 100 thousand inhabitants'},
+    'bubble_chart_title_param': {u'Ru' : u'Количество ДТП, число пострадавших и численность населения регионов с %s по %s года', u'En' : u'The number of road accidents, number of injured and the population of regions from %s to %s for year'},
+    'year_title': {u'Ru' : u'Год', u'En' : u'Year'},
+    'population_title': {u'Ru' : u'Численность населения', u'En': u'Population'},
+    'graph_type_title': {u'Ru' : u'Тип графика', u'En': u'Type of graph'},
+    'trend_line_title': {u'Ru' : u'Линия тренда', u'En': u'Trend line'},
+    'reason_title': {u'Ru' : u'Причина ДТП', u'En': u'The reason for road accident'},
+    'observed_title': {u'Ru' :u'Наблюдаемая величина', u'En': u'Observed value'},
+    'trend_reason_title': {u'Ru': u'%s %s по причине \"%s\" с %s по %s года', u'En': u'%s %s because of  \"%s\" from %s to %s year'},
+    'pie_chart_title_param' :  {u'Ru' : u'Процентное соотношение числа пострадавших по причинам ДТП с %s по %s года', u'En': u'Percentage ratio of the number of injured for reasons of road accidents from %s to %s year'},
+    'injured_title': {u'Ru' : u'Число пострадавших', u'En': u'The number of injured'},
+    'sankey_title_param':  {u'Ru' : u'Потоковая диаграмма причин ДТП пропорционально их количеству с %s по %s года', u'En': u'Sankey chart of reasons of road accidents in proportion to their number from %s to %s year'},
+    'driver_pedestrian_title': {u'Ru' : u'ДТП по вине водителей и пешеходов', u'En': u'Road accidents caused by drivers and pedestrians'},
+    'diff_fin_rus_title': {u'Ru' : u'Сравнение со статистикой ДТП в Финляндии', u'En': u'Comparison of road accident statistics in Finland'},
+    'scale_title': {u'Ru' : u'Масштаб', u'En': u'Scale'},
+    'diff_fin_param_title': {u'Ru' : u'%s %s с %s по %s года', u'En': u'%s %s from %s to %s year'},
+
+}
